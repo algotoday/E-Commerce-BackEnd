9a8cd8637fc03ef9e77423a0c7f50e0600666f7b
algotoday <100335717+algotoday@users.noreply.github.com>
2022-08-16 21:19:11 -0700

    enhancements

---
+++ models/index.js
@@ -0,0 +1,55 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+})
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: {
+          model: ProductTag,
+          attributes: ['id', 'product_id', 'tag_id'],
+        }
+      }
+   ]
+  }).then(dbTagData => res.json(dbTagData))
+  .catch(err => res.status(500).json(err));
+});
+
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: {
+          model: ProductTag,
+          attributes: ['id', 'product_id', 'tag_id'],
+        }
+      }
+   ],
+   where: {
+     id: req.params.id
+   }
+  })
+  .then(dbTagData=> {
+    if(!dbTagData){
+      res.status(404).json({ message: 'No tag found with this id' });
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(dbTagData => res.json(dbTagData))
+  .catch(err => res.status(500).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbTagData => {
+    if(!dbTagData){
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+   .catch(err => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if(!dbTagData){
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
